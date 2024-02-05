cf11a6fe24bd3c6fedb66cca06985d401ba2409c
advic <advic@DESKTOP-P4E3E0L>
2024-02-04 19:46:37 -0600

    Add project files.

---
+++ ScarySpringMan/Patches/SpringManAIPatch.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+
+namespace ScarySpringMan.Patches
+{
+    [HarmonyPatch(typeof(SpringManAI))]
+    internal class SpringManAIPatch : EnemyAI
+    {
+
+        private static System.Random rnd = new System.Random();
+        private static Stopwatch stopwatch = new Stopwatch();
+        private static bool isTimerSet = false;
+        private static bool flag = false;
+        
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patchUpdate(SpringManAI __instance)
+        {
+            if(!isTimerSet)
+            {
+                stopwatch.Start();
+                isTimerSet = true;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (__instance.PlayerIsTargetable(StartOfRound.Instance.allPlayerScripts[i]) && StartOfRound.Instance.allPlayerScripts[i].HasLineOfSightToPosition(__instance.transform.position + Vector3.up * 1.6f, 68f) &&Vector3.Distance(StartOfRound.Instance.allPlayerScripts[i].gameplayCamera.transform.position, __instance.eye.position) > 0.3f)
+                {
+                    flag = true;
+                }
+
+            }
+            if (flag && stopwatch.ElapsedMilliseconds > 15000)
+            {
+                int num = rnd.Next(1, 3);
+                if (num == 2)
+                {
+                    __instance.SetAnimationGoServerRpc();
+                    stopwatch.Restart();
+                }
+            }
+
+        }
+    }
+}
---
+++ ScarySpringMan/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ScarySpringMan.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScarySpringMan
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ScarySpringManBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Goobius.ScarySpringMan";
+        private const string modName = "Scary Spring Man";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ScarySpringManBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Scary Spring Man is ALIVE :)");
+
+            harmony.PatchAll(typeof(ScarySpringManBase));
+            harmony.PatchAll(typeof(SpringManAIPatch));
+
+        }
+
+    }
+}
