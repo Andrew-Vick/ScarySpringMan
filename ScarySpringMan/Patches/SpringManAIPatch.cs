d1846cb64a8cafdb0406abd9425f647f3643fb40
advic <vickandrew093@gmail.com>
2024-02-09 23:38:14 -0600

    new and improved

--- ScarySpringMan/Patches/SpringManAIPatch.cs
+++ ScarySpringMan/Patches/SpringManAIPatch.cs
@@ -34,6 +34,7 @@ namespace ScarySpringMan.Patches
         [HarmonyPostfix]
         static void patchUpdate(SpringManAI __instance)
         {
+
             if (!isTimerSet)
             {
                 stopwatch.Start();
@@ -41,61 +42,69 @@ namespace ScarySpringMan.Patches
                 ScarySpringManBase.mls.LogInfo("Timer start");
             }
 
-            flag = false;
-            amountOfPlayers = 0;
-
-            for (int i = 0; i < 4; i++) // i relates to clientId everyone gets one when they join starting at 0 for host
+            if (ShouldStartMoving(__instance))
             {
-                if (__instance.PlayerIsTargetable(StartOfRound.Instance.allPlayerScripts[i]) && StartOfRound.Instance.allPlayerScripts[i].HasLineOfSightToPosition(__instance.transform.position + Vector3.up * 1.6f, 68f) && Vector3.Distance(StartOfRound.Instance.allPlayerScripts[i].gameplayCamera.transform.position, __instance.eye.position) > 0.3f)
+                StartMovingServerRpc(__instance);
+            }
+        }
+
+        static bool ShouldStartMoving(SpringManAI __instance)
+        {
+            bool flag = false;
+            int amountOfPlayers = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (__instance.PlayerIsTargetable(StartOfRound.Instance.allPlayerScripts[i]) &&
+                    StartOfRound.Instance.allPlayerScripts[i].HasLineOfSightToPosition(__instance.transform.position + Vector3.up * 1.6f, 68f) &&
+                    Vector3.Distance(StartOfRound.Instance.allPlayerScripts[i].gameplayCamera.transform.position, __instance.eye.position) > 0.3f)
                 {
                     flag = true;
                     amountOfPlayers++;
                 }
-
             }
-            if (flag && stopwatch.ElapsedMilliseconds > 1000)
+
+            if (flag && stopwatch.ElapsedMilliseconds > 5000)
             {
-                int num = rnd.Next(1, 50);    // add random num stuff here currently set to 2 for testing
-                if (num == 25)
+                int num = rnd.Next(1, 500);
+                if (num == 250)
                 {
-                    StartMovingCoroutineServerRpc(__instance);  // Call the serverRpc to begine movement
                     stopwatch.Restart();
+                    return true;
                 }
             }
+            return false;
         }
         // read it and weep with me
 
         /** Use Coroutine for smooth movement of enemies. Since it takes longer than one frame for the SpringMan to move I needed something that acted as a loop 
          * but wasn't a direct loop as that doesn't allow for Unity to update and render the rest of the game(freezes your game until done). Coroutine allows for 
          * the movement to be handle as a sort of loop but still gives control back to Unity to allow for it to render and do other tasks. **/
-
-        public static void StartMovingCoroutineServerRpc(SpringManAI __instance)
+        [ServerRpc]
+        public static void StartMovingServerRpc(SpringManAI __instance)
         {
-            __instance.StartCoroutine(MoveTowardsPlayerServerRpc(__instance));
+            if (NetworkManager.Singleton.IsServer) // Double-checking server authority
+            {
+                __instance.StartCoroutine(MoveTowardsPlayer(__instance));
+            }
         }
         // Need to tone down speed and distance moved as its a bit buggy in the current state with my Rpc handling
-        [ServerRpc(RequireOwnership = false)]
-        private static IEnumerator MoveTowardsPlayerServerRpc(SpringManAI __instance)
+        private static IEnumerator MoveTowardsPlayer(SpringManAI __instance)
         {
-            if (!NetworkManager.Singleton.IsServer) // Ensure we're on the server
-            {
-                yield break; // Exit if not on the server
-            }
 
             float currentSpeed = 0f;
-            float targetSpeed = 5f;
-            float acceleration = 1.5f;
+            float targetSpeed = 10f;
+            float acceleration = 2.5f;
 
             Vector3 startPosition = __instance.transform.position;
             Vector3 targetPosition = GameNetworkManager.Instance.localPlayerController.transform.position;
 
-            __instance.agent.SetDestination(targetPosition);
-
             float totalDistance = Vector3.Distance(targetPosition, startPosition);
             float distanceMoved = 0f;
 
-            while (!__instance.agent.pathPending && distanceMoved < totalDistance * 0.10f)
+            while (!__instance.agent.pathPending && distanceMoved < totalDistance * 0.25f)
             {
+                __instance.agent.SetDestination(targetPosition);
                 currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, acceleration * Time.deltaTime);
 
                 float step = currentSpeed * Time.deltaTime;
@@ -103,8 +112,6 @@ namespace ScarySpringMan.Patches
                 __instance.transform.position = newPosition; // NetworkTransform synchronizes this change
 
                 __instance.agent.speed = currentSpeed;
-                __instance.creatureAnimator.SetFloat("walkSpeed", currentSpeed);
-                UpdateAnimationClientRpc(__instance, currentSpeed);
 
                 distanceMoved += step; // Update the distance moved
                 Vector3 targetDirection = targetPosition - __instance.transform.position;
@@ -113,12 +120,18 @@ namespace ScarySpringMan.Patches
                     Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
                     __instance.transform.rotation = Quaternion.Slerp(__instance.transform.rotation, targetRotation, Time.deltaTime * __instance.agent.angularSpeed);
                 }
+                UpdateAnimationClientRpc(__instance, currentSpeed);
                 yield return null;
             }
             __instance.agent.isStopped = true;
             __instance.agent.speed = 0;
-            UpdateAnimationClientRpc(__instance, 0f);
 
+        }
+
+        [ClientRpc]
+        public static void moveClientRpc(SpringManAI __instance)
+        {
+            __instance.StartCoroutine(MoveTowardsPlayer(__instance));
         }
 
         // Believe this may be redundant as the coroutine should update the postion with NetworkTransorm via __instance.transform.position
